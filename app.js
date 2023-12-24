dc47ab9ecfbcdcd7940df088981702575c3e6944
shivraj2710 <shivraj@compose.co.in>
2023-12-24 11:07:23 +0530

    invoice generator

---
+++ app.js
@@ -0,0 +1,65 @@
+// IMPORT PACKAGES
+import express from 'express';
+import easyinvoice from 'easyinvoice';
+import fs from 'fs';
+import path from 'path';
+
+// APP
+const app = express();
+
+// IMAGE PATH
+let imgPath = path.resolve('img', 'invoice.png');
+
+// Function to encode file data to base64 encoded string
+function base64_encode(img) {
+    // read binary data
+    let png = fs.readFileSync(img);
+    // convert binary data to base64 encoded string
+    return Buffer.from(png).toString('base64');
+}
+
+// DATA OBJECT (Moved outside the invoicePdf function for reusability)
+let data = {
+    // ... (your existing data object)
+};
+
+// INVOICE PDF FUNCTION
+const invoicePdf = async (invoiceData) => {
+    // Create your invoice! Easy!
+    let result = await easyinvoice.createInvoice(invoiceData);
+    const pdfPath = `./invoice/invoice${Date.now()}.pdf`;
+    fs.writeFileSync(pdfPath, result.pdf, 'base64');
+    return pdfPath;
+};
+
+// Define a route for PDF creation
+app.post('/create-pdf', async (req, res) => {
+    try {
+        // Extract product data from request body
+        const products = req.body.products;
+
+        // Update data object with the received product data
+        const invoiceData = {
+            ...data,
+            products,
+        };
+
+        // Create PDF
+        const pdfPath = await invoicePdf(invoiceData);
+
+        // Send the created PDF to the client
+        res.download(pdfPath, 'invoice.pdf', () => {
+            // Delete the PDF after it has been sent to the client
+            fs.unlinkSync(pdfPath);
+        });
+    } catch (error) {
+        console.error('Error creating PDF:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// SERVER LISTENER
+let PORT = 3001;
+app.listen(PORT, () => {
+    console.log('Server beating...');
+});
